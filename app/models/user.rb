9be7d76e90c787c717126b9b45159b00433e08f3
Jean-Baptiste Brenaut <jean-baptiste.brenaut@epitech.eu>
2014-03-25 19:22:48 +0000

    Added validations for User model
    * Added validation for login
    * Added validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     devise :database_authenticatable,
         :rememberable, :trackable, :confirmable, :lockable
+
+    validates :login, presence: true
+    validates :email, presence: true, format: Devise.email_regexp
 end
