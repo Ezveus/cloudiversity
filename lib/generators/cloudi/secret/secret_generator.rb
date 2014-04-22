88ec5848ab2a48e5f8393f11779d383136afa07d
Jean-Baptiste Brenaut <jean-baptiste.brenaut@epitech.eu>
2014-04-22 15:53:26 +0100

    Updated secret generator
    * rails g secret is now rails g cloudi:secret

---
+++ lib/generators/cloudi/secret/secret_generator.rb
@@ -0,0 +1,11 @@
+module Cloudi
+    class SecretGenerator < Rails::Generators::NamedBase
+        source_root File.expand_path('../templates', __FILE__)
+        # this is to prevent rails generate from complaining about missing name
+        argument :name, type: :string, default: "secret"
+
+        def generate_secret
+            create_file "config/initializers/secret_token.rb", "Cloudiversity::Application.config.secret_key_base = '#{`rake secret`.chomp}'"
+        end
+    end
+end
