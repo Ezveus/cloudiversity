52280628f049a2d0804700fb603913421a3b56bc
Jean-Baptiste Brenaut <jean-baptiste.brenaut@epitech.eu>
2014-07-30 20:24:12 +0200

    Fixed '/version'
    Summary: * Fixes T78
    
    Test Plan: GET '/version' without being authenticated
    
    Reviewers: adaedra, ezveus
    
    Reviewed By: ezveus
    
    Maniphest Tasks: T78
    
    Differential Revision: https://phabricator.adaedra.eu/D46

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
     skip_after_action :verify_authorized
     skip_after_action :verify_policy_scoped
 
-    skip_filter :authenticate_entity_from_token!, :authenticate_entity!, only: [ :version ]
+    skip_filter :authenticate_user_from_token!, :authenticate_user!, only: [ :version ]
 
     def home
     end
